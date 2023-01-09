b645efca523b866454c132d44303cd240a2b1077
Solomon Great <greatsoma2019@gmail.com>
2023-01-09 10:37:00 +0100

    Update main.js

--- main.js
+++ main.js
@@ -9,25 +9,24 @@ const arg1 = document.getElementById("arg-one"),
   opt3 = document.getElementById("option-three");
 let randNum = Math.floor(Math.random() * 4);
 let rand1 = Math.floor(Math.random() * 999);
-  let rand2 = Math.floor(Math.random() * 999);
-  let num1, num2;
-  if (rand1 < rand2) {
+let rand2 = Math.floor(Math.random() * 999);
+let num1, num2;
+if (rand1 < rand2) {
     num1 = rand2;
     num2 = rand1;
     console.log("num1 was less");
-  } else {
+} else {
     num1 = rand1;
     num2 = rand2;
-  }
+}
+arg1.textContent=num1;
+arg2.textContent=num2;
+console.log(arg1,arg2)
+function add(){
+  op.innerHTML=`<i class="fa-solid fa-plus"></i>`
+  
+}
 
-function add() {}
-
-const setValues = () => {
-  arg1.textContent = num1;
-  arg2.textContent = num2;
-};
-setValues()
 function getOperation(){
     
 }
-function add()
