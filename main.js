daf5c7c9dfbaa46351eef0cc60c28315720d982d
Solomon Great <greatsoma2019@gmail.com>
2023-01-10 12:56:05 +0100

    Update main.js

--- main.js
+++ main.js
@@ -17,28 +17,23 @@ let correctCount = 0,
   wrongCount = 0;
 // html elements////
 
-if (rand1 < rand2) {
-  num1 = rand2[Math.floor(Math.random() * rand2.length)];
-  num2 = rand1;
-} else {
-  num1 = rand1;
-  num2 = rand2[Math.floor(Math.random() * rand2.length)];
-}
-arg1.textContent = num1;
-arg2.textContent = num2;
+const main = () => {
+  //main function
+  let randNum;
+  let rand1 = Math.floor(Math.random() * 999);
+  let rand2 = [11, rand1];
+  let num1, num2, result;
+  let countdownCount = 20;
 
-function add() {
-  op.innerHTML = `<i class="fa-solid fa-plus">+</i>`;
-  result = num1 + num2;
-}
-function multiply() {
-  op.innerHTML = `<i class="fa-solid fa-xmark">*</i>`;
-  result = num1 * num2;
-}
-function subtract() {
-  op.innerHTML = `<i class="fa-solid fa-minus">-</i>`;
-  result = num1 - num2;
-}
+  if (rand1 < rand2) {
+    num1 = rand2[Math.floor(Math.random() * rand2.length)];
+    num2 = rand1;
+  } else {
+    num1 = rand1;
+    num2 = rand2[Math.floor(Math.random() * rand2.length)];
+  }
+  arg1.textContent = num1;
+  arg2.textContent = num2;
 
 function getOperation() {
   randNum = Math.floor(Math.random() * 4);
